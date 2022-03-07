358d1cd3f77a7f0553c80e5bffdb7a995352fd87
Igor Frolov <moodtraffic@gmail.com>
2022-03-07 18:14:41 +0300

    Add program - compare two numbers

---
+++ Program_2/Program.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * @author Igor Frolov <moodtraffic@gmail.com>
+ *
+ * Принимает два числа от пользователя
+ * и выводит информацию о том, какое число больше
+ */
+
+int firstNumber;
+int secondNumber;
+
+Console.Write("Inpur first number: ");
+
+firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Inpur second number: ");
+secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+if (firstNumber == secondNumber) {
+    Console.WriteLine("Numbers are equal");
+} else if (firstNumber > secondNumber) {
+    Console.WriteLine("The first number is greater than second number");
+} else {
+    Console.WriteLine("The second number is greater than first number");
+}
+
+Console.WriteLine();
