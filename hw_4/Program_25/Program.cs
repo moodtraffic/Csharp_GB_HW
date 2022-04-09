0043d95bc40038222e29ff725a2911ef33e1392f
Igor Frolov <moodtraffic@gmail.com>
2022-04-09 23:24:30 +0300

    Add Program_25

---
+++ hw_4/Program_25/Program.cs
@@ -0,0 +1,44 @@
+/**
+ * @author Igor Frolov <moodtraffic@gmail.com>
+ *
+ * Принимает на вход натуральное число A и натуральное число B. Выводит результат возведения
+ * числа A в степень B.
+ */
+
+int numberA = 0;
+int numberB = 0;
+
+// введен ли первый аргумент? число A
+if (args.Length >= 1) {
+    numberA = int.Parse(args[0]);
+} else {
+    Console.Write("Input number A: ");
+    numberA = Convert.ToInt32(Console.ReadLine());
+}
+
+// введен ли второй агрумент? число B
+if (args.Length >= 2) {
+    numberB = int.Parse(args[1]);
+} else {
+   Console.Write("Input number B: ");
+   numberB = Convert.ToInt32(Console.ReadLine());
+   Console.WriteLine();
+}
+
+double Result = 1; // степень 0
+
+if (numberB > 1) {
+    for (int i = 0; i < numberB; i++) {
+        Result *= numberA;
+    }
+} else if (numberB < 0) {
+    for (int i = numberB; i < 0; i++) {
+        Result = Result / numberA;
+    }
+} else if (numberB == 1) {
+    Result = numberA;
+}
+
+Console.WriteLine($"A = {numberA} and B = {numberB}");
+Console.WriteLine($"A^B = {Result}");
+Console.WriteLine();
