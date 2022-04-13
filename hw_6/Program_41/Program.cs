0b689da605c12a4f24cc08f8540719ad2baff110
Igor Frolov <moodtraffic@gmail.com>
2022-04-13 23:54:15 +0300

    Program_41 | Add inputNumber method

--- hw_6/Program_41/Program.cs
+++ hw_6/Program_41/Program.cs
@@ -5,8 +5,6 @@
  * Посчитайте, сколько чисел больше 0 ввёл пользователь.
  */
 
-int mode;
-
 int[] modes = {1, 2, 3, 0};
 string[] menus = {
     "Load from file",
@@ -16,17 +14,36 @@ string[] menus = {
 };
 
 printMenu();
-chooseMode();
+int mode = chooseMode();
+
+int numberCount = 0;
+
+switch (mode) {
+    case 1:
+        // read file
+        // fill array from file
+        break;
+
+    case 2:
+        numberCount = inputNumber("Set array size: ", 1);
+        // запросить с клавиатуры
+        break;
+
+    case 3:
+        numberCount = inputNumber("Set array size: ", 1);
+        // сгенерировать массив
+        break;
+}
+
+if (numberCount < 1) {
+    Console.WriteLine();
+    Environment.Exit(0);
+}
+
+// посчитать сколько числе больше 0
 
 /******************** [ functions ] ****************************************/
 
-// узнать кол-во числел у полльзователя
-
-// сгенерировать массив
-
-// запросить с клавиатуры
-
-// посчитать сколько числе больше 0
 
 void printMenu()
 {
@@ -38,13 +55,48 @@ void printMenu()
     Console.WriteLine();
 }
 
-void chooseMode()
+int chooseMode()
 {
-    do {
-        Console.Write("Choose an action: ");
+    int mode;
 
-        if (int.TryParse(Console.ReadLine(), out mode) && modes.Contains(mode)) {
+    do {
+        mode = inputNumber("Choose an action: ");
+    } while(!modes.Contains(mode));
+
+    return mode;
+}
+
+int inputNumber(string message, int? MinValue = null, int? MaxValue = null)
+{
+    string? errorMessage = null;
+    int number = 0;
+
+    if (MinValue == null && MaxValue == null) {
+        MinValue = Int32.MinValue;
+        MaxValue = Int32.MaxValue;
+    } else if (MinValue == null) {
+        MinValue = Int32.MinValue;
+
+        errorMessage = $"Number should be ≤ {MaxValue}";
+    } else if (MaxValue == null) {
+        MaxValue = Int32.MaxValue;
+
+        errorMessage = $"Number should be ≥ {MinValue}";
+    } else {
+        errorMessage = $"Number should be betwean {MinValue} and {MaxValue}";
+    }
+
+    do {
+        Console.Write(message);
+
+        if (int.TryParse(Console.ReadLine(), out number) && number >= MinValue && number <= MaxValue) {
             break;
         }
+
+        if (null != errorMessage) {
+            Console.WriteLine(errorMessage);
+        }
     } while(true);
+
+    return number;
 }
