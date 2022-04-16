425db0cdf0e56dc9a240be104af0d282000505f0
Igor Frolov <moodtraffic@gmail.com>
2022-04-17 00:18:52 +0300

    Program_62 | Make Maxtri class ; add method Maxtrix.fill

--- hw_9/Program_62/Program.cs
+++ hw_9/Program_62/Program.cs
@@ -5,7 +5,7 @@
  * по спирали числами от 1 до M x N.
  */
 
-int M = 6; // столбцы
+int M = 5; // столбцы
 int N = 5; // строки
 
 Matrix matrix = new Matrix(N, M);
@@ -19,26 +19,41 @@ public class Matrix
     protected int number = 0;
     protected int[,] Values;
 
-    public Matrix(int cols, int rows)
+    public Matrix(int rows, int cols)
     {
         this.cols = cols;
         this.rows = rows;
         this.Values = new int[rows, cols];
     }
 
-    public (int, int) fill(int x0, int y0, int? cols = null, int? rows = null)
+    public void fill(int x0 = 0, int y0 = 0, int? rows = null, int? cols = null)
     {
+        int steps = 0;
         int cells = 0;
-        int number = 0;
 
         cols = (cols == null) ? this.cols : cols;
         rows = (rows == null) ? this.rows : rows;
 
-        do {
-            number = this.getNextValue();
-        } while (++cells < rows * cols);
+        if (cols == 1 && rows == 1) {
+            cells = 1;
+        } else if (cols == 1) {
+            cells = (int)rows;
+        } else if (rows == 1) {
+            cells = (int)cols;
+        } else {
+            cells = 2* (int)cols + 2* (int)rows -4;
+        }
 
-        return (x0, y0);
+        Console.WriteLine($"perimeter = {cells}");
+
+        do {
+            int number = this.getNextValue();
+            Console.WriteLine(number);
+        } while (++steps < cells);
+
+        if (rows -2 > 0 && cols -2 > 0) {
+            this.fill(x0, y0, rows -2, cols -2);
+        }
     }
 
     protected int getNextValue()
